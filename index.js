e29f56789660fd1cfec80f48cab192cae3330db3
Bruno Shema <shemab71@gmail.com>
2024-05-02 08:38:25 +0300

    second change

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const username=document.getElementById(`username`).value;
 const password=document.getElementById(`password`).value;
 const login=document.getElementById(`login`);
 const out=document.getElementById(`out`);
+let word="hello";
 
 out.textContent= "username: "+username +"\t"+ "password: "+password;
 
 for(let i=0; i<5; i++){
-    out.textContent= "hello world";
+    out.textContent= word.charAt(i);
+    console.log(word.charAt(i));
 }
